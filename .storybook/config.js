9ee1ee4db3eefb8bbfd98929ade5483786d1fceb
Angus Fretwell <angusfretwell@me.com>
2018-02-02 10:39:08 +1100

    add knobs and config storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,18 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: '🦐 Roo UI',
+  addonPanelInRight: true,
+});
 
 const req = require.context('../packages', true, /story.js$/);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
 }
+
+addDecorator(withKnobs);
 
 configure(loadStories, module);
