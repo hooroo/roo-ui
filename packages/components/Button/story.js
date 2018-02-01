92d2984335f954b4db54a4acafc1efbc5aeef8d3
Angus Fretwell <angusfretwell@me.com>
2018-02-02 10:25:27 +1100

    add storybook readme addon

--- packages/components/Button/story.js
+++ packages/components/Button/story.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withDocs } from 'storybook-readme';
 
 import Button from './src';
+import README from './README.md';
 
 storiesOf('Button', module)
+  .addDecorator(withDocs(README))
   .add('default', () => (
     <Button>Hello world!</Button>
   ));
