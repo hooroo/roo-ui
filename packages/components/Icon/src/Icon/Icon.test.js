59d5b9cc36ceb81ca213c60cdc884b72ba5f671d
allanpope <pope.allans@gmail.com>
2018-02-02 15:44:52 +1100

    Fix tests

--- packages/components/Icon/src/Icon/Icon.test.js
+++ packages/components/Icon/src/Icon/Icon.test.js
@@ -1,62 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Icon from '.';
-import qantasPaths from './qantasPaths.json';
+import { Icon } from '.';
 
 describe('<Icon />', () => {
   let wrapper;
+  const iconPath = 'M12.272 25.036l-8.749-8.49L0 20.026 12.272 32 36 8.482 32.474 5z';
 
   it('renders correctly', () => {
-    expect(shallow(<Icon />)).toMatchSnapshot();
+    expect(shallow(<Icon path={iconPath} />)).toMatchSnapshot();
   });
 
-  describe('defaults', () => {
-    beforeEach(() => {
-      wrapper = shallow(<Icon />);
-    });
-
-    it('renders the <path /> for the default icon', () => {
-      expect(wrapper.find('path').props()).toHaveProperty('d', paths.location);
-    });
-
-    it('sets the data-id attribute of the <svg />', () => {
-      expect(wrapper.find('svg').props()).toHaveProperty('data-id', 'icon-location');
-    });
-
-    it('renders a <title /> with default text', () => {
-      expect(wrapper.find('title').text()).toBe('Location icon');
-    });
-
-    it('sets the width and height attributes of the <svg />', () => {
-      expect(wrapper.find('svg').props()).toMatchObject({
-        height: '1em',
-        width: '1em',
-      });
-    });
-
-    it('sets the fill attribute <svg />', () => {
-      expect(wrapper.find('svg').props()).toHaveProperty('fill', 'currentColor');
-    });
-  });
-
-  describe('with the name prop', () => {
-    beforeEach(() => {
-      wrapper = shallow(<Icon name="food" />);
-    });
-
-    it('renders the <path /> for the icon', () => {
-      expect(wrapper.find('path').props()).toHaveProperty('d', paths.food);
-    });
-
-    it('sets the data-id attribute of the <svg /> with the name', () => {
-      expect(wrapper.find('svg').props()).toHaveProperty('data-id', 'icon-food');
-    });
+  it('renders the <path /> for the provided icon', () => {
+    wrapper = shallow(<Icon path={iconPath} />);
+    expect(wrapper.find('path').props()).toHaveProperty('d', iconPath);
   });
 
   describe('with the size prop', () => {
     beforeEach(() => {
-      wrapper = shallow(<Icon size="3rem" />);
+      wrapper = shallow(<Icon path={iconPath} size="3rem" />);
     });
 
     it('sets the width and height attributes of the <svg /> with the size', () => {
@@ -69,7 +31,7 @@ describe('<Icon />', () => {
 
   describe('with the width prop', () => {
     beforeEach(() => {
-      wrapper = shallow(<Icon width="3rem" />);
+      wrapper = shallow(<Icon path={iconPath} width="3rem" />);
     });
 
     it('sets the width attribute of the <svg />', () => {
@@ -82,7 +44,7 @@ describe('<Icon />', () => {
 
   describe('with the height prop', () => {
     beforeEach(() => {
-      wrapper = shallow(<Icon height="3rem" />);
+      wrapper = shallow(<Icon path={iconPath} height="3rem" />);
     });
 
     it('sets the height attribute of the <svg />', () => {
@@ -95,7 +57,7 @@ describe('<Icon />', () => {
 
   describe('with the title prop', () => {
     beforeEach(() => {
-      wrapper = shallow(<Icon title="An accessible title" />);
+      wrapper = shallow(<Icon path={iconPath} title="An accessible title" />);
     });
 
     it('renders a <title /> with the title text', () => {
@@ -105,7 +67,7 @@ describe('<Icon />', () => {
 
   describe('with the fill prop', () => {
     beforeEach(() => {
-      wrapper = shallow(<Icon fill="#c00" />);
+      wrapper = shallow(<Icon path={iconPath} fill="#c00" />);
     });
 
     it('sets the fill attribute of the <svg />', () => {
