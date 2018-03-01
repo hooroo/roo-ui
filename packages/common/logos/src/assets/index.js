e43b29633b3ef78587fb61d5585e3040e0a46163
allanpope <pope.allans@gmail.com>
2018-03-01 16:33:32 +1100

    Add tripadvisor logos

--- packages/common/logos/src/assets/index.js
+++ packages/common/logos/src/assets/index.js
@@ -1,7 +1,12 @@
 import qantas from './qantas.svg';
 import jetstar from './jetstar.svg';
+import tripadvisor from './tripadvisor.svg';
+import tripadvisorOwl from './tripadvisor-owl.svg';
+
 
 export {
   qantas,
   jetstar,
+  tripadvisor,
+  tripadvisorOwl,
 };
