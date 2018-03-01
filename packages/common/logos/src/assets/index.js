e22287fb08f4df4d924a01d0aa54203081d4d7a5
Allan Pope <allan.pope@hooroo.com>
2018-03-02 09:55:31 +1100

    feat(Logo): add more logos (#5)
    * Add Trip Advisor logo(main one and owl)
    * Add Oneworld logo

--- packages/common/logos/src/assets/index.js
+++ packages/common/logos/src/assets/index.js
@@ -1,7 +1,13 @@
 import qantas from './qantas.svg';
 import jetstar from './jetstar.svg';
+import tripAdvisor from './trip-advisor.svg';
+import tripAdvisorOwl from './trip-advisor-owl.svg';
+import oneworld from './oneworld.svg';
 
 export {
   qantas,
   jetstar,
+  tripAdvisor,
+  tripAdvisorOwl,
+  oneworld,
 };
