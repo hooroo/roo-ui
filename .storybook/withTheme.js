3273f7b53512c9553de64f4447aedcfde9ad8e3d
Angus Fretwell <angusfretwell@me.com>
2018-04-09 19:53:55 +1000

    chore: use src rather than dist themes in storybook

--- .storybook/withTheme.js
+++ .storybook/withTheme.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { select } from '@storybook/addon-knobs/react';
 
-import * as themes from '../packages/common/themes';
+import * as themes from '../packages/common/themes/src';
 
 const selectedTheme = () => select('Theme', {
   qantas: 'Qantas',
