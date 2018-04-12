c6a95bf3512b9d73070664abe42612efc843f5ee
Angus Fretwell <angusfretwell@me.com>
2018-04-12 12:29:22 +1000

    test: add outline button tests

---
+++ packages/components/Button/src/OutlineButton/OutlineButton.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { qantas as theme } from '@roo-ui/themes';
+import { shallowWithTheme } from '@roo-ui/test-utils';
+
+import OutlineButton from '.';
+
+describe('<OutlineButton />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowWithTheme(
+      <OutlineButton rounded>Hello world</OutlineButton>,
+      theme,
+    );
+  });
+
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('primary', () => {
+    beforeEach(() => {
+      wrapper = shallowWithTheme(
+        <OutlineButton primary>Hello world</OutlineButton>,
+        theme,
+      );
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('rounded', () => {
+    beforeEach(() => {
+      wrapper = shallowWithTheme(
+        <OutlineButton rounded>Hello world</OutlineButton>,
+        theme,
+      );
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
