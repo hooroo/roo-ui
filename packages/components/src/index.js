7859ac42c63a309e12c4ebd3c5c5364feda23cb6
Angus Fretwell <angusfretwell@me.com>
2018-05-07 15:20:13 +1000

    feat: add input and label components

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -6,6 +6,8 @@ export { default as Container } from './Container';
 export { default as Flex } from './Flex';
 export { default as Heading } from './Heading';
 export { default as Image } from './Image';
+export { default as Input } from './Input';
+export { default as Label } from './Label';
 export { default as Link } from './Link';
 export { default as BlockLink } from './BlockLink';
 export { default as OutlineButton } from './OutlineButton';
