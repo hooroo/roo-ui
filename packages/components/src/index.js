581dbbc3b0ea28dd1a53375da70d79e968a92e50
Angus Fretwell <angusfretwell@me.com>
2018-05-07 12:09:08 +1000

    chore: update components index

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,7 +1,13 @@
+export { default as BackgroundImage } from './BackgroundImage';
+export { default as BlockLink } from './BlockLink';
+export { default as Box } from './Box';
 export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Container } from './Container';
+export { default as Flex } from './Flex';
 export { default as Heading } from './Heading';
+export { default as Image } from './Image';
 export { default as Link } from './Link';
 export { default as OutlineButton } from './OutlineButton';
 export { default as Text } from './Text';
-export { default as Flex } from './Flex';
-export { default as Box } from './Box';
+export { default as Truncate } from './Truncate';
