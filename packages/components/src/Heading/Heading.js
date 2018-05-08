35c3db658a2821efa3696e97bd3f8811955feec3
Angus Fretwell <angusfretwell@me.com>
2018-05-08 14:17:41 +1000

    refactor: update font sizes

--- packages/components/src/Heading/Heading.js
+++ packages/components/src/Heading/Heading.js
@@ -11,40 +11,40 @@ Heading.defaultProps = {
 Heading.h1 = Heading.withComponent('h1');
 Heading.h1.defaultProps = {
   ...Heading.defaultProps,
-  fontSize: 'xxxxl',
+  fontSize: 'xxxxxl',
 };
 
 Heading.h2 = Heading.withComponent('h2');
 Heading.h2.defaultProps = {
   ...Heading.defaultProps,
-  fontSize: 'xl',
+  fontSize: 'xxl',
 };
 
 Heading.h3 = Heading.withComponent('h3');
 Heading.h3.defaultProps = {
   ...Heading.defaultProps,
-  fontSize: 'lg',
+  fontSize: 'xl',
   lineHeight: 'loose',
 };
 
 Heading.h4 = Heading.withComponent('h4');
 Heading.h4.defaultProps = {
   ...Heading.defaultProps,
-  fontSize: 'base',
+  fontSize: 'lg',
   lineHeight: 'loose',
 };
 
 Heading.h5 = Heading.withComponent('h5');
 Heading.h5.defaultProps = {
   ...Heading.defaultProps,
-  fontSize: 'sm',
+  fontSize: 'base',
   lineHeight: 'loose',
 };
 
 Heading.h6 = Heading.withComponent('h6');
 Heading.h6.defaultProps = {
   ...Heading.defaultProps,
-  fontSize: 'xs',
+  fontSize: 'sm',
   lineHeight: 'loose',
 };
 
