6be3adb5bf461b6b5eb4065dddfbb79079dcf98e
Angus Fretwell <angusfretwell@me.com>
2018-05-10 14:48:16 +1000

    feat: add additional props

--- packages/components/src/Link/Link.js
+++ packages/components/src/Link/Link.js
@@ -1,25 +1,43 @@
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 import tag from 'clean-tag';
-import { color, hover } from 'styled-system';
+import { color, hover, fontWeight, themeGet } from 'styled-system';
 
 const Link = styled(tag.a)`
   cursor: pointer;
   text-decoration: none;
+  color: ${themeGet('colors.ui.link')};
+
+  &:hover {
+    color: ${themeGet('colors.ui.linkHover')};
+  }
+
+  &:focus {
+    outline: ${themeGet('borders.2')} ${themeGet('colors.brand.secondary')};
+  }
 
   ${color}
   ${hover}
+  ${fontWeight}
+
+  ${props => props.inline && css`
+    &, &:hover {
+      text-decoration: underline;
+      color: inherit;
+    }
+  `}
 `;
 
 Link.propTypes = {
   ...color.propTypes,
   ...hover.propTypes,
+  ...fontWeight.propTypes,
+  inline: PropTypes.boolean,
 };
 
 Link.defaultProps = {
-  color: 'ui.link',
-  hover: {
-    color: 'ui.linkHover',
-  },
+  blacklist: Object.keys(Link.propTypes),
+  inline: false,
 };
 
 export default Link;
