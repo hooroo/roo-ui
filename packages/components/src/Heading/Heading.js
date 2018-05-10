b10e1755a9f0e5cb190315d1f68c97add3763520
Angus Fretwell <angusfretwell@me.com>
2018-05-10 13:05:03 +1000

    fix: use clean tag for headings

--- packages/components/src/Heading/Heading.js
+++ packages/components/src/Heading/Heading.js
@@ -1,6 +1,8 @@
+import tag from 'clean-tag';
+
 import Text from '../Text';
 
-const Heading = Text.withComponent('span');
+const Heading = Text.withComponent(tag.span);
 
 Heading.defaultProps = {
   ...Text.defaultProps,
@@ -8,40 +10,40 @@ Heading.defaultProps = {
   fontSize: 'lg',
 };
 
-Heading.h1 = Heading.withComponent('h1');
+Heading.h1 = Heading.withComponent(tag.h1);
 Heading.h1.defaultProps = {
   ...Heading.defaultProps,
   fontSize: 'xxxxl',
 };
 
-Heading.h2 = Heading.withComponent('h2');
+Heading.h2 = Heading.withComponent(tag.h2);
 Heading.h2.defaultProps = {
   ...Heading.defaultProps,
   fontSize: 'xl',
 };
 
-Heading.h3 = Heading.withComponent('h3');
+Heading.h3 = Heading.withComponent(tag.h3);
 Heading.h3.defaultProps = {
   ...Heading.defaultProps,
   fontSize: 'lg',
   lineHeight: 'loose',
 };
 
-Heading.h4 = Heading.withComponent('h4');
+Heading.h4 = Heading.withComponent(tag.h4);
 Heading.h4.defaultProps = {
   ...Heading.defaultProps,
   fontSize: 'base',
   lineHeight: 'loose',
 };
 
-Heading.h5 = Heading.withComponent('h5');
+Heading.h5 = Heading.withComponent(tag.h5);
 Heading.h5.defaultProps = {
   ...Heading.defaultProps,
   fontSize: 'sm',
   lineHeight: 'loose',
 };
 
-Heading.h6 = Heading.withComponent('h6');
+Heading.h6 = Heading.withComponent(tag.h6);
 Heading.h6.defaultProps = {
   ...Heading.defaultProps,
   fontSize: 'xs',
