c1656ead1ffa8822d93171cf2fb687aa23cbbe73
Eamon Hyland <ehyland90@gmail.com>
2018-05-11 10:59:00 +1000

    Define all top level theme properties as const

--- packages/themes/src/qantas.js
+++ packages/themes/src/qantas.js
@@ -27,6 +27,8 @@ const colors = {
   text: '#323232',
 };
 
+const fontFamily = 'QantasCiutadella, sans-serif';
+
 const fontSizes = {
   sm: rem('14px'),
   base: rem('16px'),
@@ -46,6 +48,11 @@ const lineHeights = {
 const fontWeights = {
   normal: 500,
   bold: 600,
+};
+
+const letterSpacings = {
+  normal: '0',
+  wide: '0.125em',
 };
 
 const textStyles = {
@@ -91,62 +98,75 @@ const textStyles = {
   },
 };
 
+const breakpoints = ['40rem', '52rem', '64rem'];
+
+const maxWidths = {
+  default: rem('1280px'),
+};
+
+const space = [
+  rem('0px'),
+  rem('4px'),
+  rem('8px'),
+  rem('12px'),
+  rem('16px'),
+  rem('24px'),
+  rem('32px'),
+  rem('64px'),
+  rem('128px'),
+  rem('256px'),
+  rem('512px'),
+];
+
+const borders = [
+  0,
+  '1px solid',
+  '2px solid',
+  '3px solid',
+];
+
+const radii = {
+  none: 0,
+  default: '4px',
+  rounded: '1000px',
+};
+
+const shadows = {
+  none: 0,
+  default: '0 1px 1px 0 rgba(0, 0, 0, 0.1)',
+  heavy: '0 2px 2px 0 rgba(0, 0, 0, 0.1)',
+  focus: `0 0 0 2px ${colors.brand.secondary};`,
+};
+
+const transitions = {
+  default: '200ms ease-in',
+};
+
+const buttons = {
+  default: {
+    color: 'white',
+    backgroundColor: colors.grey[0],
+  },
+  primary: {
+    color: 'white',
+    backgroundColor: colors.brand.primary,
+  },
+};
+
 export default {
   colors,
-  breakpoints: ['40rem', '52rem', '64rem'],
-  space: [
-    rem('0px'),
-    rem('4px'),
-    rem('8px'),
-    rem('12px'),
-    rem('16px'),
-    rem('24px'),
-    rem('32px'),
-    rem('64px'),
-    rem('128px'),
-    rem('256px'),
-    rem('512px'),
-  ],
-  maxWidths: {
-    default: rem('1280px'),
-  },
-  borders: [
-    0,
-    '1px solid',
-    '2px solid',
-    '3px solid',
-  ],
-  radii: {
-    none: 0,
-    default: '4px',
-    rounded: '1000px',
-  },
-  shadows: {
-    none: 0,
-    default: '0 1px 1px 0 rgba(0, 0, 0, 0.1)',
-    heavy: '0 2px 2px 0 rgba(0, 0, 0, 0.1)',
-    focus: `0 0 0 2px ${colors.brand.secondary};`,
-  },
-  transitions: {
-    default: '200ms ease-in',
-  },
-  fontFamily: 'QantasCiutadella, sans-serif',
+  breakpoints,
+  space,
+  maxWidths,
+  borders,
+  radii,
+  shadows,
+  transitions,
+  fontFamily,
   fontSizes,
   lineHeights,
   fontWeights,
   textStyles,
-  letterSpacings: {
-    normal: '0',
-    wide: '0.125em',
-  },
-  buttons: {
-    default: {
-      color: 'white',
-      backgroundColor: colors.grey[0],
-    },
-    primary: {
-      color: 'white',
-      backgroundColor: colors.brand.primary,
-    },
-  },
+  letterSpacings,
+  buttons,
 };
