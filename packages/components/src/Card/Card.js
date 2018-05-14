312434c5c2a4cef27ef6655ed9b758094c11067b
Eamon Hyland <ehyland90@gmail.com>
2018-05-14 15:21:15 +1000

    Add display, flexDirection & flexWrap to Card

--- packages/components/src/Card/Card.js
+++ packages/components/src/Card/Card.js
@@ -1,12 +1,28 @@
+import {
+  display,
+  flexDirection,
+  flexWrap,
+} from 'styled-system';
 import { Box } from '..';
 
-const Card = Box.extend``;
+const Card = Box.extend`
+  ${display}
+  ${flexDirection}
+  ${flexWrap}
+`;
 
 Card.defaultProps = {
   boxShadow: 'default',
   borderRadius: 'default',
   bg: 'white',
   p: 4,
+};
+
+Card.propTypes = {
+  ...Box.propTypes,
+  ...display.propTypes,
+  ...flexDirection.propTypes,
+  ...flexWrap.propTypes,
 };
 
 export default Card;
