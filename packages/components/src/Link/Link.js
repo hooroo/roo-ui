308f15a532fec6ac125f3c13427f21150ce95290
Doug McClurg <doug.mcclurg@gmail.com>
2018-05-14 12:05:40 +1000

    feat: adds hiddenVisually to link

--- packages/components/src/Link/Link.js
+++ packages/components/src/Link/Link.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
+import { hideVisually } from 'polished';
 import tag from 'clean-tag';
 import { color, hover, fontWeight, themeGet } from 'styled-system';
 
@@ -26,18 +27,24 @@ const Link = styled(tag.a)`
       color: inherit;
     }
   `}
+
+  ${props => props.hidden && css`
+    ${hideVisually()}
+  `}
 `;
 
 Link.propTypes = {
   ...color.propTypes,
   ...hover.propTypes,
   ...fontWeight.propTypes,
+  hidden: PropTypes.bool,
   inline: PropTypes.bool,
 };
 
 Link.defaultProps = {
   blacklist: Object.keys(Link.propTypes),
   inline: false,
+  hidden: false,
 };
 
 export default Link;
