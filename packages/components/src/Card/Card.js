8f89192f77491124dd18702305ef5b26f88ae5ae
Eamon Hyland <ehyland90@gmail.com>
2018-05-15 13:21:06 +1000

    Revert "Add display, flexDirection & flexWrap to Card"
    This reverts commit 312434c5c2a4cef27ef6655ed9b758094c11067b.

--- packages/components/src/Card/Card.js
+++ packages/components/src/Card/Card.js
@@ -1,28 +1,12 @@
-import {
-  display,
-  flexDirection,
-  flexWrap,
-} from 'styled-system';
 import { Box } from '..';
 
-const Card = Box.extend`
-  ${display}
-  ${flexDirection}
-  ${flexWrap}
-`;
+const Card = Box.extend``;
 
 Card.defaultProps = {
   boxShadow: 'default',
   borderRadius: 'default',
   bg: 'white',
   p: 4,
-};
-
-Card.propTypes = {
-  ...Box.propTypes,
-  ...display.propTypes,
-  ...flexDirection.propTypes,
-  ...flexWrap.propTypes,
 };
 
 export default Card;
