99f5e861491d79ac1b4057c01728abafad733f04
Doug McClurg <doug.mcclurg@gmail.com>
2018-05-16 08:02:08 +1000

    feat: Add paragraph Text style, set default Text style to span

--- packages/components/src/Text/Text.story.js
+++ packages/components/src/Text/Text.story.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import { withDocs } from 'storybook-readme';
 
@@ -8,10 +8,15 @@ import README from './README.md';
 storiesOf('Components|Text', module)
   .addDecorator(withDocs(README))
   .add('default', () => (
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget eros
-      malesuada, suscipit turpis id, vehicula lorem. Etiam iaculis mi quis risus
-      mattis posuere. Cras ultricies metus mauris, a dapibus lacus facilisis
-      iaculis. Proin bibendum ultricies viverra.
-    </Text>
+    <Fragment>
+      <Text>
+        Generic text.
+      </Text>
+      <Text.paragraph>
+        A paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget eros
+        malesuada, suscipit turpis id, vehicula lorem. Etiam iaculis mi quis risus
+        mattis posuere. Cras ultricies metus mauris, a dapibus lacus facilisis
+        iaculis.
+      </Text.paragraph>
+    </Fragment>
   ));
