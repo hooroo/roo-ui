90237ea2c34547cddfffba9b07f03e015d4f9f92
Eamon Hyland <ehyland90@gmail.com>
2018-05-16 11:50:19 +1000

    Set base font styles using inject globa

--- packages/themes/src/ThemeProvider.js
+++ packages/themes/src/ThemeProvider.js
@@ -1,30 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled, { ThemeProvider as Provider } from 'styled-components';
+import { ThemeProvider, injectGlobal } from 'styled-components';
 import { themeGet } from 'styled-system';
 
-export const Base = styled.div`
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
-  font-family: ${themeGet('fontFamily')};
-  font-size: ${themeGet('fontSizes.base')};
-  line-height: ${themeGet('lineHeights.normal')};
-  color: ${themeGet('colors.text')};
+injectGlobal`
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  
+    font-family: ${themeGet('fontFamily')};
+    font-size: ${themeGet('fontSizes.base')};
+    line-height: ${themeGet('lineHeights.normal')};
+    color: ${themeGet('colors.text')};
+  }
 
   *, *:before, *:after {
     box-sizing: border-box;
   }
 `;
-
-const ThemeProvider = ({ theme, ...props }) => (
-  <Provider theme={theme}>
-    <Base {...props} />
-  </Provider>
-);
-
-ThemeProvider.propTypes = {
-  theme: PropTypes.shape({}).isRequired,
-};
 
 export default ThemeProvider;
