c96f0d0408cb5c29cd07822602f39e8b4a32647f
allanpope <pope.allans@gmail.com>
2018-05-23 16:33:45 +1000

    feat: add read only input style

--- packages/components/src/Input/Input.story.js
+++ packages/components/src/Input/Input.story.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withDocs } from 'storybook-readme';
+import { boolean } from '@storybook/addon-knobs/react';
 
 import Input from '.';
 import README from './README.md';
@@ -8,5 +9,9 @@ import README from './README.md';
 storiesOf('Components|Input', module)
   .addDecorator(withDocs(README))
   .add('default', () => (
-    <Input placeholder="Hello world" />
+    <Input
+      placeholder="Hello world"
+      error={boolean('Error', false)}
+      readOnly={boolean('Read only', false)}
+    />
   ));
