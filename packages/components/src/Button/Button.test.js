9c85670798be86df0de93b357a510a6771cf18f2
allanpope <pope.allans@gmail.com>
2018-05-25 15:46:26 +1000

    Add accessibility test to each component

--- packages/components/src/Button/Button.test.js
+++ packages/components/src/Button/Button.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { qantas as theme } from '@roo-ui/themes';
 import { shallowWithTheme } from '@roo-ui/test-utils';
+import { axe } from 'jest-axe';
 
 import Button from '.';
 
@@ -13,6 +14,10 @@ describe('<Button />', () => {
 
   it('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('has no accessibility errors', async () => {
+    expect(await axe(wrapper.html())).toHaveNoViolations();
   });
 
   describe('primary', () => {
--- packages/components/src/Input/Input.test.js
+++ packages/components/src/Input/Input.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { qantas as theme } from '@roo-ui/themes';
 import { shallowWithTheme } from '@roo-ui/test-utils';
+import { axe } from 'jest-axe';
 
 import Input from '.';
 
@@ -13,5 +14,10 @@ describe('<Input />', () => {
 
   it('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('has no accessibility errors', async () => {
+    wrapper = shallowWithTheme(<label htmlFor="input">Label<Input /></label>, theme);
+    expect(await axe(wrapper.html())).toHaveNoViolations();
   });
 });
