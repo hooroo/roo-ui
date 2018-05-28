a986056e4faf6be8d9e76c822a3dd34c5b33815e
Allan Pope <allan.pope@hooroo.com>
2018-05-29 09:52:54 +1000

    test: add accessibility tests (#54)
    * Add  and setup jest-axe library
    
    * Add default empty alt tag to images
    
    * Add accessibility test to each component
    
    * Add a11y test to star rating

--- packages/components/src/ExternalLink/ExternalLink.test.js
+++ packages/components/src/ExternalLink/ExternalLink.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { qantas as theme } from '@roo-ui/themes';
 import { shallowWithTheme } from '@roo-ui/test-utils';
+import { axe } from 'jest-axe';
 
 import ExternalLink from '.';
 
@@ -13,5 +14,9 @@ describe('<ExternalLink />', () => {
 
   it('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('has no accessibility errors', async () => {
+    expect(await axe(wrapper.html())).toHaveNoViolations();
   });
 });
