b028b21a344b75bd4999881c021e1424a9a38461
allanpope <pope.allans@gmail.com>
2018-05-28 15:13:50 +1000

    Add ErrorMessage

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -3,6 +3,7 @@ export { default as Box } from './Box';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Container } from './Container';
+export { default as ErrorMessage } from './ErrorMessage';
 export { default as Flex } from './Flex';
 export { default as Heading } from './Heading';
 export { default as Hide } from './Hide';
