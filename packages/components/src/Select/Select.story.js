917f312b02ad5c0c49fc46deb50bb180e178f07a
Jonathan Ly <jonathan.ly@hooroo.com>
2018-06-13 11:25:41 +1000

    feat: add select component (#75)
    * refactor: add white background color to Input
    
    * feat: add select component
    
    * test: update Textarea snapshots

---
+++ packages/components/src/Select/Select.story.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withDocs } from 'storybook-readme';
+import { boolean } from '@storybook/addon-knobs/react';
+
+import Select from '.';
+import README from './README.md';
+
+storiesOf('Components|Select', module)
+  .addDecorator(withDocs(README))
+  .add('default', () => (
+    <Select error={boolean('Error', false)}>
+      <option>One</option>
+      <option>Two</option>
+      <option>Three</option>
+    </Select>
+  ));
