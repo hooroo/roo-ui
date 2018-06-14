8db69eb0355d0b04cf5434ef66e3effd1f28dce2
jonathanly <jonathanlyhv@gmail.com>
2018-06-14 16:52:57 +1000

    refactor: add padding to checkbox icon

--- packages/components/src/Checkbox/Checkbox.js
+++ packages/components/src/Checkbox/Checkbox.js
@@ -58,6 +58,7 @@ const CheckboxBorder = styled(tag.div)`
 const CheckboxIcon = Icon.extend`
   position: absolute;
   display: none;
+  padding: ${themeGet('space.1')};
   top: 0;
   left: -${rem('2px')};
 
