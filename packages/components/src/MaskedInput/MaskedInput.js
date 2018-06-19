f13a429ce3596fc92c4ec2ebb45076762b587c7f
jonathanly <jonathanlyhv@gmail.com>
2018-06-19 11:45:33 +1000

    Revert "fix: add workaround to update masked input values (#86)"
    This reverts commit acedb3696fce8602b2e0198537e02455e9426db0.

--- packages/components/src/MaskedInput/MaskedInput.js
+++ packages/components/src/MaskedInput/MaskedInput.js
@@ -1,27 +1,9 @@
-import React from 'react';
 import TextMask from 'react-text-mask';
-import cleanElement from 'clean-element';
 
 import { Input } from '..';
 
-const CleanInput = cleanElement('input');
-
-CleanInput.propTypes = {
-  ...Input.propTypes,
-};
-
 const MaskedInput = Input.withComponent(TextMask);
 MaskedInput.displayName = 'MaskedInput';
-MaskedInput.defaultProps = {
-  ...Input.defaultProps,
-  render: (ref, { defaultValue, ...props }) => (
-    <CleanInput
-      ref={ref}
-      value={defaultValue}
-      {...props}
-    />
-  ),
-};
 
 MaskedInput.time = MaskedInput.extend``;
 MaskedInput.time.displayName = 'MaskedInput.time';
