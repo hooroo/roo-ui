ce0684e64f9ab5fcfce54006d40358cb3a790e16
allanpope <pope.allans@gmail.com>
2018-06-19 14:58:31 +1000

    Add hidden prop to visually hide text

--- packages/components/src/Text/Text.story.js
+++ packages/components/src/Text/Text.story.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withDocs } from 'storybook-readme';
+import { boolean } from '@storybook/addon-knobs/react';
 
 import Text from '.';
 import README from './README.md';
@@ -8,7 +9,7 @@ import README from './README.md';
 storiesOf('Components|Text', module)
   .addDecorator(withDocs(README))
   .add('default', () => (
-    <Text>
+    <Text hidden={boolean('Hidden', false)} >
       Generic text.
     </Text>
   ));
