fff3e4a3ace089f752266938b9484d1eaaa6803f
Doug McClurg <doug.mcclurg@gmail.com>
2018-06-20 10:46:02 +1000

    add axe accesibility test

--- packages/components/src/LoadingIndicator/LoadingIndicator.test.js
+++ packages/components/src/LoadingIndicator/LoadingIndicator.test.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { axe } from 'jest-axe';
 import LoadingIndicator from './LoadingIndicator';
 
 describe('<LoadingIndicator />', () => {
   let props;
+  let wrapper;
 
   beforeEach(() => {
     props = {};
+    wrapper = shallow(<LoadingIndicator {...props} />).dive();
   });
 
-  const render = () => shallow(<LoadingIndicator {...props} />).dive();
-
   it('matches snapshot', () => {
-    expect(render()).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('has no accessibility errors', async () => {
+    expect(await axe(wrapper.html())).toHaveNoViolations();
   });
 });
