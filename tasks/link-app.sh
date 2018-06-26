aa0e906c6cf738a1654cd6c0b314586d3b24e9e0
Allan Pope <allan.pope@hooroo.com>
2018-06-26 16:45:30 +1000

    chore: add script to link roo-ui to a local app (#97)
    * Support linking roo-ui to a local app
    
    * Update snapshots
    
    * Remove redundant version
    
    * Move workflow instructions into contributing docs
    
    * Re add version
    
    * Add script to link and unlink roo-ui to a project
    
    * Remove echo since to link command outputs what is happening
    
    * Add license to packages to remove noise from the warning

---
+++ tasks/link-app.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+packages=($(yarn lerna ls | grep @roo-ui))
+packages+=('styled-components')
+
+yarn lerna exec 'yarn link'
+
+cd $1
+
+for package in ${packages[@]/*v*/}; do
+  yarn link "$package"
+done
