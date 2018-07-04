fdbb3c1c17ff1643772b379523620184acad9dab
allanpope <pope.allans@gmail.com>
2018-07-04 16:59:40 +1000

    Simplify story

--- packages/components/src/Calendar/Calendar.story.js
+++ packages/components/src/Calendar/Calendar.story.js
@@ -2,26 +2,21 @@
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { number, date } from '@storybook/addon-knobs';
+import { number } from '@storybook/addon-knobs';
 import { withDocs } from 'storybook-readme';
 import { addDays, subDays } from 'date-fns';
 
 import Calendar from './Calendar';
 import README from './README.md';
 
-function dateKnob(name, defaultValue) {
-  const stringTimestamp = date(name, defaultValue);
-  return new Date(stringTimestamp);
-}
-
 storiesOf('Components|Calendar', module)
   .addDecorator(withDocs(README))
   .add('default', () => (
     <Calendar
       monthsToDisplay={number('Months to display', 1)}
-      minDate={dateKnob('Min date', subDays(new Date(), 20))}
-      maxDate={dateKnob('Max date', addDays(new Date(), 60))}
-      selected={dateKnob('Selected date', new Date())}
+      minDate={subDays(new Date(), 10)}
+      maxDate={addDays(new Date(), 60)}
+      selected={new Date()}
       onDateSelected={console.log}
     />
   ));
