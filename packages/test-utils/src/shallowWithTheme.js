19db234c9b72b8ab54008d2f5324e9c0b4e4e5a8
jonathanly <jonathanlyhv@gmail.com>
2018-08-14 13:40:06 +1000

    Pass theme to child components; update snapshots

--- packages/test-utils/src/shallowWithTheme.js
+++ packages/test-utils/src/shallowWithTheme.js
@@ -3,6 +3,12 @@ import { shallow } from 'enzyme';
 import { ThemeProvider } from 'styled-components';
 
 export default (tree, theme) => {
-  const context = shallow(<ThemeProvider theme={theme} />).instance().getChildContext();
-  return shallow(tree, { context });
+  const context = shallow(<ThemeProvider theme={theme} />)
+    .instance()
+    .getChildContext();
+
+  return shallow(tree, {
+    context,
+    childContextTypes: ThemeProvider.childContextTypes,
+  });
 };
