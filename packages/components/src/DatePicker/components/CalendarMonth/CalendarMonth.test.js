7b565c51c6caaa1d948c56ed8e9fc1bf10df3f90
Kamal Rizk <kimomicho@gmail.com>
2018-08-14 11:31:14 +1000

    refactor: rename Calendar component to DatePicker (#140)

---
+++ packages/components/src/DatePicker/components/CalendarMonth/CalendarMonth.test.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { qantas as theme } from '@roo-ui/themes';
+import { shallowWithTheme } from '@roo-ui/test-utils';
+import { addDays } from 'date-fns';
+
+import CalendarMonth from '.';
+
+describe('<CalendarMonth />', () => {
+  let wrapper;
+
+  const startDate = new Date(2018, 7, 1, 10, 33, 30, 0);
+
+  const props = {
+    monthName: 'Jul',
+    month: 7,
+    year: 2018,
+    stacked: true,
+    weekdayNames: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    weeks: [
+      [
+        { date: startDate },
+        { date: addDays(startDate, 1) },
+        { date: addDays(startDate, 2) },
+        { date: addDays(startDate, 3) },
+        { date: addDays(startDate, 4) },
+        { date: addDays(startDate, 5) },
+        { date: addDays(startDate, 7) },
+      ],
+      [
+        { date: addDays(startDate, 7) },
+        { date: addDays(startDate, 8) },
+        { date: addDays(startDate, 9) },
+        { date: addDays(startDate, 10) },
+        { date: addDays(startDate, 11) },
+        { date: addDays(startDate, 12) },
+        { date: addDays(startDate, 14) },
+      ]],
+    getDateProps: jest.fn,
+  };
+
+  beforeEach(() => {
+    wrapper = shallowWithTheme(<CalendarMonth {...props}>Days</CalendarMonth>, theme);
+  });
+
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('<CalendarWeekdays />', () => {
+    it('renders CalendarWeekdays', () => {
+      expect(wrapper.find('CalendarWeekdays').prop('month')).toEqual(props.month);
+      expect(wrapper.find('CalendarWeekdays').prop('year')).toEqual(props.year);
+      expect(wrapper.find('CalendarWeekdays').prop('weekdayNames')).toEqual(props.weekdayNames);
+    });
+  });
+
+  describe('<CalendarDays />', () => {
+    it('renders CalendarDays', () => {
+      expect(wrapper.find('CalendarDays').prop('month')).toEqual(props.month);
+      expect(wrapper.find('CalendarDays').prop('year')).toEqual(props.year);
+      expect(wrapper.find('CalendarDays').prop('weeks')).toEqual(props.weeks);
+    });
+  });
+});
--- packages/components/src/DatePicker/components/CalendarWeekday/CalendarWeekday.test.js
+++ packages/components/src/DatePicker/components/CalendarWeekday/CalendarWeekday.test.js
@@ -2,13 +2,7 @@ import React from 'react';
 import { qantas as theme } from '@roo-ui/themes';
 import { shallowWithTheme } from '@roo-ui/test-utils';
 
-import { CalendarWeekday, CalendarWeekdays } from '.';
-
-describe('<CalendarWeekdays />', () => {
-  it('renders correctly', () => {
-    expect(shallowWithTheme(<CalendarWeekdays>Weekday</CalendarWeekdays>, theme)).toMatchSnapshot();
-  });
-});
+import CalendarWeekday from '.';
 
 describe('<CalendarWeekday />', () => {
   it('renders correctly', () => {
