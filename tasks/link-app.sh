c9aaa81eca213e128a83ec9b969d6e964d603cc3
Eamon Hyland <ehyland90@gmail.com>
2018-08-15 10:23:01 +1000

    Manually create styled-components link

--- tasks/link-app.sh
+++ tasks/link-app.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
-packages=($(yarn lerna ls | grep @roo-ui))
-packages+=('styled-components')
+if [ -z "$1" ]; then
+  echo "Usage: $0 [path-to-consuming-app]"
+  exit 1
+fi
 
+roo_path="$(cd `dirname $0`/.. && pwd)"
+consumer_path="$(cd $1 && pwd)"
+packages=($(yarn lerna ls | grep @roo-ui))
+
+# Create links for @roo-ui/* packages
+cd $roo_path
 yarn lerna exec 'yarn link'
 
-cd $1
-
+# Link packages in consuming app
+cd $consumer_path
 for package in ${packages[@]/*v*/}; do
   yarn link "$package"
 done
+
+# Link styled-components
+rm -rf "$consumer_path/node_modules/styled-components"
+ln -s \
+  "$roo_path/node_modules/styled-components" \
+  "$consumer_path/node_modules/styled-components"
--- tasks/unlink-app.sh
+++ tasks/unlink-app.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 [path-to-consuming-app]"
+  exit 1
+fi
+
+roo_path="$(cd `dirname $0`/.. && pwd)"
+consumer_path="$(cd $1 && pwd)"
 packages=($(yarn lerna ls | grep @roo-ui))
-packages+=('styled-components')
 
-cd $1
-
+# Remove links in comsumer
+cd $consumer_path
 for package in ${packages[@]/*v*/}; do
   yarn unlink "$package"
 done
 
+# Remove styled-components link
+rm -rf "$consumer_path/node_modules/styled-components"
+
+# Reinstall dependances
 echo "reinstall dependencies"
 yarn --check-files
 
-cd -
-
+# Remove links for @roo-ui/* packages
+cd $roo_path
 yarn lerna exec 'yarn unlink'
