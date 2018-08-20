19303c69ccaa522e283a22d7f9b4d944ddc5b8a9
Allan Pope <allan.pope@hooroo.com>
2018-08-20 11:40:08 +1000

    refactor: add generic colour names (#157)
    * Use generic color name in star rating
    
    * Remove text color key
    
    * Add generic colour names
    
    * Make color names more clear
    
    * Fix lint error
    
    * Update color names variable

--- packages/themes/src/ThemeProvider.js
+++ packages/themes/src/ThemeProvider.js
@@ -6,13 +6,13 @@ const ThemeProvider = ({ theme, ...otherProps }) => {
     body {
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
-    
+
       font-family: ${theme.fontFamily};
       font-size: ${theme.fontSizes.base};
       line-height: ${theme.lineHeights.normal};
-      color: ${theme.colors.text};
+      color: ${theme.colors.greys.charcoal};
     }
-  
+
     *, *:before, *:after {
       box-sizing: border-box;
     }
