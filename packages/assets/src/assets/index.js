f9397d9e8b31dbf889541fa7627a762591deaf2f
scara82 <marta.scaramella@gmail.com>
2018-09-20 10:34:50 +1000

    Move the Qantas alert icon in assets

--- packages/assets/src/assets/index.js
+++ packages/assets/src/assets/index.js
@@ -2,3 +2,4 @@ export { default as roo } from './roo.svg';
 export { default as rooRed } from './rooRed.svg';
 export { default as pebble } from './pebble.svg';
 export { default as pebbleRed } from './pebbleRed.svg';
+export { default as alertIllustration } from './alertIllustration.svg';
