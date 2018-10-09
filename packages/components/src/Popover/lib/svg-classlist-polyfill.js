e5d37a2d84b0d7a835307a5a8b8bedb42ae0cef0
Douglas McClurg <doug.mcclurg@gmail.com>
2018-10-09 16:09:24 +1100

    fix: Adds check for window before running svg-classlist-polyfill

--- packages/components/src/Popover/lib/svg-classlist-polyfill.js
+++ packages/components/src/Popover/lib/svg-classlist-polyfill.js
@@ -1,13 +1,15 @@
 // react-onclickoutside requires classList
 // This adds classList to SVG's to avoid errors in in IE11
-if (!('classList' in SVGElement.prototype)) {
-  Object.defineProperty(SVGElement.prototype, 'classList', {
-    get() {
-      return {
-        contains: className => (
-          this.className.baseVal.split(' ').indexOf(className) !== -1
-        ),
-      };
-    },
-  });
+if (window) {
+  if (!('classList' in SVGElement.prototype)) {
+    Object.defineProperty(SVGElement.prototype, 'classList', {
+      get() {
+        return {
+          contains: className => (
+            this.className.baseVal.split(' ').indexOf(className) !== -1
+          ),
+        };
+      },
+    });
+  }
 }
