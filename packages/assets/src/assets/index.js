317bd86ef26634ed8ed9a5ee0c72658ff4b77fde
Rob Monie <robmonie@gmail.com>
2018-10-19 13:58:50 +1100

    feat: add trip advisor rating svgs

--- packages/assets/src/assets/index.js
+++ packages/assets/src/assets/index.js
@@ -3,3 +3,6 @@ export { default as rooRed } from './rooRed.svg';
 export { default as pebble } from './pebble.svg';
 export { default as pebbleRed } from './pebbleRed.svg';
 export { default as alertIllustration } from './alertIllustration.svg';
+export { default as tripAdvisorCircle } from './tripAdvisorCircle.svg';
+export { default as tripAdvisorCircleHalf } from './tripAdvisorCircleHalf.svg';
+export { default as tripAdvisorCircleBorder } from './tripAdvisorCircleBorder.svg';
