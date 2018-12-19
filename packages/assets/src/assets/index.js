d160c53e38998543df081d488ed1815f5bd687ed
Marta <marta.scaramella@qantashotels.com>
2018-12-19 11:10:40 +1100

    feat: add pebbleRedBig and pebbleWhite svgs (#209)
    * feat: add pebbleRedBig and pebbleWhite svgs
    
    * pebbleRedBig renamed pebbleRedNarrow

--- packages/assets/src/assets/index.js
+++ packages/assets/src/assets/index.js
@@ -2,6 +2,8 @@ export { default as roo } from './roo.svg';
 export { default as rooRed } from './rooRed.svg';
 export { default as pebble } from './pebble.svg';
 export { default as pebbleRed } from './pebbleRed.svg';
+export { default as pebbleRedNarrow } from './pebbleRedNarrow.svg';
+export { default as pebbleWhite } from './pebbleWhite.svg';
 export { default as alertIllustration } from './alertIllustration.svg';
 export { default as tripAdvisorCircle } from './tripAdvisorCircle.svg';
 export { default as tripAdvisorCircleHalf } from './tripAdvisorCircleHalf.svg';
