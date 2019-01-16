5fe149bdd09a2cd3492c93395e60be1674dea77a
Angus Fretwell <angusfretwell@me.com>
2019-01-16 11:00:08 +1100

    filter theme and forwarded component from snapshots

--- setupTests.js
+++ setupTests.js
@@ -2,7 +2,17 @@ require('jest-styled-components');
 
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
+const omit = require('lodash/omit');
+const { createSerializer } = require('enzyme-to-json');
 const { toHaveNoViolations } = require('jest-axe');
+
+expect.addSnapshotSerializer(createSerializer({
+  map: node =>
+    omit(node, [
+      'props.forwardedComponent',
+      'props.theme',
+    ]),
+}));
 
 enzyme.configure({ adapter: new Adapter() });
 
