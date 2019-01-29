444ed6641d731d72ee837712b1932f267308db23
Phil <philipwindeyer@gmail.com>
2019-01-29 11:07:14 +1100

    Add "loading" prop to Button (#215)
    Add example of LoadingIndicator in Button via composition and corresponding story in docs

--- packages/components/src/Button/Button.story.js
+++ packages/components/src/Button/Button.story.js
@@ -5,6 +5,7 @@ import { boolean, select } from '@storybook/addon-knobs/react';
 
 import Button from '.';
 import README from './README.md';
+import LoadingIndicator from '../LoadingIndicator';
 
 storiesOf('Components|Button', module)
   .addDecorator(withDocs(README))
@@ -17,4 +18,17 @@ storiesOf('Components|Button', module)
     >
       Hello world
     </Button>
-  ));
+  ))
+  .add('with loading indicator', () => {
+    const isLoading = boolean('isLoading?', true);
+    return (
+      <Button
+        variant={select('Variant', ['default', 'primary'], 'default')}
+        rounded={boolean('Rounded', false)}
+        block={boolean('Block', false)}
+        disabled={isLoading}
+      >
+        {isLoading ? <LoadingIndicator color="white" size={21} /> : 'Hello world'}
+      </Button>
+    );
+  });
