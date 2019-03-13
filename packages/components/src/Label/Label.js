fbc7a712db0e1d82de3cae6f1dae669ea1b3728b
Talbet Fulthorpe <talbet.fulthorpe@gmail.com>
2019-03-13 11:43:13 +1100

    Allow label margins to be changed without extending

--- packages/components/src/Label/Label.js
+++ packages/components/src/Label/Label.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { hideVisually } from 'polished';
-import { themeGet, textStyle, space } from 'styled-system';
+import { textStyle, space } from 'styled-system';
 import tag from 'clean-tag';
 
 const Label = styled(tag.label)`
   display: block;
   width: 100%;
-  margin: 0;
-  margin-bottom: ${themeGet('space.3')};
 
   ${props => props.hidden && hideVisually()}
 
@@ -24,6 +22,8 @@ Label.propTypes = {
 
 Label.defaultProps = {
   textStyle: 'label',
+  m: 0,
+  mb: 3,
   hidden: false,
 };
 
