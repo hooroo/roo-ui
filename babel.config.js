ca2ab937c35538a391b37cbe00351bc1f0264eb9
talbet <talbet.fulthorpe@gmail.com>
2019-03-18 16:26:04 +1100

    Fix/babel 7 upgrade (#221)
    This PR upgrades babel from v6 to v7.
    
    Previously this was blocked as storybook v3 was incompatible with Babel@7, but storybook has been updated to v4. An issue with Storybook and Lerna means that we have to override the Webpack config for storybook.

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = (api) => {
+  const isTest = api.env('test');
+
+  const env = isTest ? ['@babel/preset-env', { targets: { node: 'current' } }] : '@babel/preset-env';
+
+  const presets = ['@babel/preset-react', env];
+  const plugins = [
+    'babel-plugin-styled-components',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
+};
