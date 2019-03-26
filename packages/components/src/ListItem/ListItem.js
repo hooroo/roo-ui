03c3ce1051cfd366460128ce982dd1239f8d8e51
Rob Monie <robmonie@gmail.com>
2019-03-26 15:00:43 +1100

    add space/borders to list item (#224)

--- packages/components/src/ListItem/ListItem.js
+++ packages/components/src/ListItem/ListItem.js
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
-import { themeGet } from 'styled-system';
+import { themeGet, space, borders, borderColor } from 'styled-system';
+
 import tag from 'clean-tag';
 
 const ListItem = styled(tag.li)`
   margin: 0 0 ${themeGet('space.4')};
+  ${space}
+  ${borders}
+  ${borderColor}
 `;
 
 ListItem.displayName = 'ListItem';
+
+ListItem.propTypes = {
+  ...space.propTypes,
+  ...borders.propTypes,
+  ...borderColor.propTypes,
+};
 
 export default ListItem;
