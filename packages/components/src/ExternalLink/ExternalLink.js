31a7b6f7cc3cbfba357c5dd54bc22f36e0577bdd
Talbet Fulthorpe <talbet.fulthorpe@gmail.com>
2019-08-06 14:58:56 +1000

    ensure color is overridable in ExternalLink

--- packages/components/src/ExternalLink/ExternalLink.js
+++ packages/components/src/ExternalLink/ExternalLink.js
@@ -1,16 +1,14 @@
 import styled from '@emotion/styled';
-import { themeGet } from 'styled-system';
 import { Link } from '..';
 
-const ExternalLink = styled(Link)`
-  color: ${themeGet('colors.greys.charcoal')};
-`;
+const ExternalLink = styled(Link)``;
 
 ExternalLink.defaultProps = {
   ...Link.defaultProps,
   rel: 'noopener noreferrer',
   target: '_blank',
   title: 'Link opens in new window',
+  color: 'greys.charcoal',
 };
 
 export default ExternalLink;
