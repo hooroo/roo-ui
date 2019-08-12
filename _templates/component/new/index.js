e47edacf931c9285485e372ce1a98070d3425235
Angus Fretwell <angusfretwell@me.com>
2019-08-12 14:32:25 +1000

    feat: add hygen component for module entrypoints

--- _templates/component/new/index.js
+++ _templates/component/new/index.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   params: ({ args }) => ({
     componentsRoot: path.resolve(__dirname, '../../../packages/components/src'),
-    componentPath: args.componentPath,
-    componentName: path.basename(args.componentPath),
-    componentModuleName: args.componentPath.split('/')[0],
+    componentPath: args.path,
+    componentName: path.basename(args.path),
+    componentModuleName: args.path.split('/')[0],
   }),
 };
