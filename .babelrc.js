d4c46cb3ea525899239c2a83df8b76d74bc48c89
Angus Fretwell <angusfretwell@me.com>
2019-08-12 16:51:31 +1000

    build: update babel and eslint config

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: ['@babel/react', ['@babel/env', { modules: process.env.MODULES }]],
   plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+      },
+    ],
     'emotion',
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
