56b65ab07a31b9c055e1fd995ae33c7b39c3857c
Angus Fretwell <angusfretwell@me.com>
2019-08-21 13:41:03 +1000

    fix: add missing stories

---
+++ src/logos/README.md
@@ -0,0 +1,17 @@
+# Logos
+
+<!-- STORY -->
+
+## Installation
+
+```shell
+$ yarn add @roo-ui/logos
+```
+
+## Example
+
+```js
+import { qantas } from '@roo-ui/logos';
+
+export default <img alt="Qantas" src={qantas} />;
+```
---
+++ src/logos/story.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withDocs } from 'storybook-readme';
+import { select } from '@storybook/addon-knobs/react';
+
+import { Box } from '../components';
+import * as logos from './';
+import README from './README.md';
+
+storiesOf('Foundations|Logos', module)
+  .addDecorator(withDocs(README))
+  .add('default', () => (
+    <Box p={6} bg="greys.porcelain">
+      <img
+        src={select('Logo', logos, Object.values(logos)[0])}
+        height="50"
+        alt=""
+      />
+    </Box>
+  ));
