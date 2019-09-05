97514bd2d695c79a2e47a4fa2f2b1b5b133ba042
Philip Windeyer <philipwindeyer@gmail.com>
2019-09-05 18:13:22 +1000

    chore: add typedef for assets dir

---
+++ src/assets/index.d.ts
@@ -0,0 +1,9 @@
+declare module 'roo-ui/assets' {
+  export const roo: string;
+  export const rooRed: string;
+  export const pebble: string;
+  export const pebbleRed: string;
+  export const pebbleRedNarrow: string;
+  export const pebbleWhite: string;
+  export const alertIllustration: string;
+}
