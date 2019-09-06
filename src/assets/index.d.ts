ea9cfade7e10c37079ddaf883f773a1d2fbf3345
Philip Windeyer <philipwindeyer@gmail.com>
2019-09-06 10:54:54 +1000

    chore: add react types. define types for assets, icons and logos

--- src/assets/index.d.ts
+++ src/assets/index.d.ts
@@ -1,3 +1,5 @@
+import react from 'react';
+
 declare module 'roo-ui/assets' {
   export const roo: string;
   export const rooRed: string;
---
+++ src/logos/index.d.ts
@@ -0,0 +1,14 @@
+import react from 'react';
+
+declare module '@roo-ui/logos' {
+  export const qantas: string;
+  export const qantasGroup: string;
+  export const qantasHotels: string;
+  export const qantasTail: string;
+  export const jetstar: string;
+  export const oneworld: string;
+  export const qantasQff: string;
+  export const qantasBusinessRewards: string;
+  export const tripAdvisor: string;
+  export const tripAdvisorOwl: string;
+}
