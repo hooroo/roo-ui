73614cc3bcf998e496228924f65ed8c8d5309cf6
Talbet Fulthorpe <talbet.fulthorpe@qantashotels.com>
2019-10-02 16:04:47 +1000

    fix: update Flex component not to use default props

--- src/components/Flex/Flex.js
+++ src/components/Flex/Flex.js
@@ -1,20 +1,49 @@
 import styled from '@emotion/styled';
-import { system } from 'styled-system';
-import { propType } from '@styled-system/prop-types';
-import Box from '../Box';
+import {
+  space,
+  layout,
+  position,
+  color,
+  typography,
+  shadow,
+  border,
+  flexbox,
+  system,
+  compose,
+} from 'styled-system';
+import propTypes, { propType } from '@styled-system/prop-types';
 
 const customProps = system({
   flexFlow: true,
 });
 
-const Flex = styled(Box)(customProps);
+const styleProps = compose(
+  space,
+  layout,
+  position,
+  color,
+  typography,
+  shadow,
+  border,
+  flexbox,
+  customProps,
+);
+
+const Flex = styled('div')`
+  display: flex;
+  ${styleProps};
+`;
 
 Flex.propTypes = {
+  ...propTypes.space,
+  ...propTypes.layout,
+  ...propTypes.position,
+  ...propTypes.color,
+  ...propTypes.typography,
+  ...propTypes.shadow,
+  ...propTypes.border,
+  ...propTypes.flexbox,
   flexFlow: propType,
-};
-
-Flex.defaultProps = {
-  display: 'flex',
 };
 
 export default Flex;
