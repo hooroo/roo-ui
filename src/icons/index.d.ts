965fc0bc4f9cec54bdc6108cda536616862f2c69
scara82 <marta.scaramella@gmail.com>
2019-11-27 13:39:42 +1100

    feat(icon): change name from qff_check_circle to confirmation

--- src/icons/index.d.ts
+++ src/icons/index.d.ts
@@ -897,11 +897,11 @@ declare module "roo-ui/icons" {
   export const smokingRooms: Icon;
   export const spa: Icon;
   export const calendar: Icon;
+  export const confirmation: Icon;
   export const dial: Icon;
   export const feeFreePayment: Icon;
   export const pointsPay: Icon;
   export const pricePromise: Icon;
-  export const qffCheckCircle: Icon;
   export const qffPoints: Icon;
   export const ribbon: Icon;
   export const support: Icon;
