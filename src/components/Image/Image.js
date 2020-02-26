73a2a14aa7ccc5ea8fd67a2b6ae28f7ae6c8eff8
Jasmine Quek <jasmine.quek@qantashotels.com>
2020-02-26 15:22:20 +1100

    Apply suggestions from code review
    Co-Authored-By: Angus Fretwell <angusfretwell@me.com>

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
-import { space, size, height, width, textAlign } from 'styled-system';
+import { space, size, height, width, verticalAlign } from 'styled-system';
 
 const Image = styled.img`
   display: block;
   max-width: 100%;
   height: auto;
 
-  ${space} ${size} ${height} ${width} ${textAlign} ${props =>
+  ${space} ${size} ${height} ${width} ${verticalAlign} ${props =>
   props.inline &&
   css`
     display: inline-block;
@@ -20,7 +20,7 @@ Image.propTypes = {
   ...size.propTypes,
   ...height.propTypes,
   ...width.propTypes,
-  ...textAlign.propTypes,
+  ...verticalAlign.propTypes,
   inline: PropTypes.bool,
 };
 
