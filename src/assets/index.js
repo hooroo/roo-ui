53624d125ee650bc8e6f4194acf9f38e63c38d11
Marta Scaramella <marta.scaramella@qantashotels.com>
2020-06-01 16:11:23 +1000

    feat(icons): add new icons for  qvp

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,3 +15,7 @@ export { default as mail } from './mail.svg';
 export { default as laptop } from './laptop.svg';
 export { default as browser } from './browser.svg';
 export { default as headset } from './headset.svg';
+export { default as find } from './find.svg';
+export { default as pointsPlusPay } from './points-plus-pay.svg';
+export { default as points } from './points.svg';
+export { default as shield } from './shield.svg';
