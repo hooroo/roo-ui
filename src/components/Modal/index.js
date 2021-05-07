84e5d0505cfc1896ea4bee4b677add6e66296fc0
Luke Rollans <luke@rollans.sh>
2021-05-07 18:46:49 +0800

    fix: Update export syntax

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,5 @@
-export {
-  default,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  ModalGlobalStyle,
-} from './Modal';
+export { default, ModalGlobalStyle } from './Modal';
+
+export { default as ModalHeader } from './components/ModalHeader';
+export { default as ModalBody } from './components/ModalBody';
+export { default as ModalFooter } from './components/ModalFooter';
