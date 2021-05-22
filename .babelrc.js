2b655c8736f84f546ce7e86bfb3a77beed3dcbfe
Anthropic <iamanthropic@gmail.com>
2021-05-22 15:11:32 +1000

    fix: add property-in-object loose: true

--- .babelrc.js
+++ .babelrc.js
@@ -13,6 +13,7 @@ module.exports = {
     '@babel/transform-runtime',
     ['@babel/proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     '@babel/proposal-object-rest-spread',
   ],
 };
